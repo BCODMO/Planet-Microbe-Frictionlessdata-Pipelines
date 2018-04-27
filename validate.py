519e337bf85df5ea58e5db9718e4558aeadfcc1b
Adam Shepherd <ashepherd@whoi.edu>
2018-04-27 12:19:04 -0700

    downlaod project dataset datapackage.json files

--- validate.py
+++ validate.py
@@ -1,8 +1,17 @@
 from datapackage import Package
+import os
 
 package = Package('https://www.bco-dmo.org/project/2101/datapackage.json')
-
-for i in range(len(package.resource_names)):
+if package.valid:
+  for i in range(len(package.resource_names)):
     print(package.resource_names[i])
     resource = package.get_resource(package.resource_names[i])
-    print(resource.raw_read())
+    if resource.valid:
+      socket_file = resource.raw_iter(stream=True)
+      file_data = socket_file.read()
+      if not os.path.exists(package.resource_names[i]):
+        os.makedirs(package.resource_names[i])
+      filename = os.path.join(package.resource_names[i], os.path.basename(resource.source))
+      with open(filename, 'wb') as ofile:
+        ofile.write(file_data)
+      print('Wrote: ' + filename)
